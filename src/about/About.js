4ea86c8958c2e63be8b4d9b2682ea33c64f42e2c
Josh Lawley <73124061+LawleyJ@users.noreply.github.com>
2023-11-15 22:04:40 -0700

    navMenu background and profile

---
+++ src/about/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import AboutMenu from "./AboutMenu.js"
+
+export default class About extends Component {
+  render() {
+    return (
+      <>
+        
+        <AboutMenu />
+
+      </>
+    )
+  }
+}
---
+++ src/playerStats/PlayerStats.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/playerStats.css";
+
+const PlayerStats = () => {
+    return (
+        <div id="playerStats">
+            <h1>Joshua Lawley</h1>
+            <div id="playerStats-lines">
+                <span id="playerStats-lines-thick"></span>
+                <span id="playerStats-lines-thin"></span>
+            </div>
+            <h2>Software Engineer</h2>
+        </div>
+    )
+};
+
+export default PlayerStats;
