a78f3fb05dcdcf1a106bf8176c1ef30f471c42d7
Josh Lawley <73124061+LawleyJ@users.noreply.github.com>
2023-11-18 12:17:37 -0700

    Projects_and_Contact

---
+++ src/projects/projectsData.js
@@ -0,0 +1,44 @@
+import projectOne from "../assets/project-1.png";
+import projectTwo from "../assets/project-2.png";
+import projectThree from "../assets/project-3.png";
+
+const projects = {
+    1: {
+        title: "Social Media Platform",
+        image: projectOne,
+        description: (
+            <>
+                <p>                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                </p>
+            </>
+        ),
+        github: "https://github.com",
+        demo: "https://github.com",
+    },
+    2: {
+        title: "E-commerce Website",
+        image: projectTwo,
+        description: (
+            <>
+                <p>                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                </p>
+            </>
+        ),
+        github: "https://github.com",
+        demo: "https://github.com",
+    },
+    3: {
+        title: "Spotify Replica",
+        image: projectThree,
+        description: (
+            <>
+                <p>                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                </p>
+            </>
+        ),
+        github: "https://github.com",
+        demo: "https://github.com",
+    },
+}
+
+export default projects;
